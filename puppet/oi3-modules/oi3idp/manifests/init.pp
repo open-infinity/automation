cefd5867bfb7e79282a0e6b0eff73dc770e19fd4
Vedran Bartonicek <vbartoni@gmail.com>
2015-06-01 13:33:33 +0300

    Idp: dependecy to java, oi3jetty

--- puppet/oi3-modules/oi3idp/manifests/init.pp
+++ puppet/oi3-modules/oi3idp/manifests/init.pp
@@ -1,7 +1,14 @@
-class oi3idp {
+/*class oi3idp {
 	exec { "Selfsigned certificate for Idp":
 		path => "/usr/bin:/bin",
 		command => "/etc/puppet/modules/oi3idp/files/create_certs.sh to.be.configured /opt/shibboleth-idp password",
 		creates => "/opt/shibboleth-idp/credentials/idp.jks",
 	}
 }
+*/
+
+package { ["java-1.7.0-openjdk", "oi3-jetty"]:
+	ensure => present,
+	require => Class["oi3-basic"],
+}
+
