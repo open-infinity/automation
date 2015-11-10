21efb15b373760c9d98985d7fa24d25ca8e183e4
pekkaraty <pekka.raty@tieto.com>
2015-11-10 10:48:30 +0200

    typo fix

--- puppet/oi4-modules/oi4-serviceplatform/manifests/install.pp
+++ puppet/oi4-modules/oi4-serviceplatform/manifests/install.pp
@@ -1,6 +1,6 @@
 class oi4-serviceplatform::install {
 package { ["java-1.8.0-openjdk", "oi4-serviceplatform", "oi4-bas"]:
-	ensure => present,4
+	ensure => present,
 	require => Class['oi4-activiti-rdbms'],
 }
 
