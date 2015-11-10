db76fff0345af9402ead0ea633d410ac82689d82
pekkaraty <pekka.raty@tieto.com>
2015-11-10 13:36:36 +0200

    trying to fix dependency cycle by removing '->'  dependency markers: https://tickets.puppetlabs.com/browse/MODULES-1493

--- puppet/oi4-modules/profiles/manifests/bas.pp
+++ puppet/oi4-modules/profiles/manifests/bas.pp
@@ -27,40 +27,41 @@ class profiles::bas {
     bas_multicast_address       => $multicast_address,
     bas_tomcat_monitor_role_pwd => $tomcat_monitor_role_password,
   }->
+  class {'oi4bas::service':
+  }
+  
   tomcat::config::server::valve { 'securityvault-valve':
     class_name    => 'org.openinfinity.sso.valve.AttributeBasedSecurityVaultValve',
     catalina_base => "$oi_home/tomcat",
-  }->
+  }
   tomcat::setenv::entry { 'catalina_out':
     param => 'CATALINA_OUT',
     value => "$oi_home/og/tomcat/catalina.out",
-  }->
+  }
   tomcat::setenv::entry {'jmx_opts':
     param => 'JMX_OPTS',
     value => "-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=65329 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=true -Dcom.sun.management.jmxremote.password.file=$oi_home/tomcat/conf/jmxremote.password -Dcom.sun.management.jmxremote.access.file=$oi_home/tomcat/conf/jmxremote.access",
     order => 1,
-  }->
+  }
   tomcat::setenv::entry { 'extra_jvm_opts':
     param => 'EXTRA_JVM_OPTS',
     value => $extra_jvm_opts,
     order => 2,
-  }->
+  }
   tomcat::setenv::entry { 'extra_catalina_opts':
     param => 'EXTRA_CATALINA_OPTS',
     value => $extra_catalina_opts,
     order => 3,
-  }->
+  }
   tomcat::setenv::entry { 'java_opts':
     param => 'JAVA_OPTS',
     value => "\$JAVA_OPTS -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=EET -Dfile.encoding=UTF8 -Xmx${jvm_mem}m -XX:MaxPermSize=${jvm_perm}m -Dsecurity.vault.properties.file=$oi_home/tomcat/conf/securityvault.properties \$JMX_OPTS \$EXTRA_JVM_OPTS",
     order => 10,
-  }->
+  }
   tomcat::setenv::entry { 'catalina_opts':
     param => 'CATALINA_OPTS',
     value => '$CATALINA_OPTS $JMX_OPTS $EXTRA_CATALINA_OPTS',
     order => 11,
-  }->
-  class {'oi4bas::service':
   }
 
 }
