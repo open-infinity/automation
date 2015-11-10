4215630f1118135c9af43f216c464720df376ca9
pekkaraty <pekka.raty@tieto.com>
2015-11-10 11:24:04 +0200

    conditional oi4-bas check

--- puppet/oi4-modules/oi4-serviceplatform/manifests/install.pp
+++ puppet/oi4-modules/oi4-serviceplatform/manifests/install.pp
@@ -1,7 +1,10 @@
 class oi4-serviceplatform::install {
-package { ["java-1.8.0-openjdk", "oi4-serviceplatform", "oi4-bas"]:
-	ensure => present,
-	require => Class['oi4-activiti-rdbms'],
+package { ["java-1.8.0-openjdk", "oi4-serviceplatform"]:
+	ensure => present
+}
+
+if ! defined(Package['oi4-bas']) {
+    package { 'oi4-bas': ensure => present }
 }
 
 }
