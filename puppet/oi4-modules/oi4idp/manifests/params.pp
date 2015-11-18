70d45208f385d4329a3fa47fe29d7e135e28069d
Ville Korhonen <ville.korhonen@tieto.com>
2015-11-18 13:11:45 +0200

    Fix to hieradata references.

--- puppet/oi4-modules/oi4idp/manifests/params.pp
+++ puppet/oi4-modules/oi4idp/manifests/params.pp
@@ -1,31 +1,32 @@
 class oi4idp::params {
         include stdlib
 
-	# Static
+	# Should be from Hiera
+	# Static(?)
 	$java_home='/usr/lib/jvm/jre'
 	$dot='.'
 	$platform_home_prefix = hiera('toas::idp::platform_home_prefix')
-	$platform_home_suffix = hiera('toas::idp::platform_home_prefix')
-	$platform_home = hiera('toas::idp::platform_home_prefix')
-	$platform_install_path_prefix=hiera('toas::idp::platform_home_prefix')
-	$platform_install_path=hiera('toas::idp::platform_home_prefix')
-	$idp_shibboleth_idp_dir_prefix=hiera('toas::idp::platform_home_prefix')
-	$idp_install_path=hiera('toas::idp::platform_home_prefix')
-	$idp_path=hiera('toas::idp::platform_home_prefix')
-	$idp_rpm_name=hiera('toas::idp::platform_home_prefix')
-	$idp_rpm=hiera('toas::idp::platform_home_prefix')
-	$idp_install_script_prefix=hiera('toas::idp::platform_home_prefix')
-	$idp_install_script_conf_file=hiera('toas::idp::platform_home_prefix')
-	$idp_install_script=hiera('toas::idp::platform_home_prefix')
-	$apacheds_rpm=hiera('toas::idp::platform_home_prefix')
+	$platform_home_suffix = hiera('toas::idp::platform_home_suffix')
+	$platform_home = hiera('toas::idp::platform_home')
+	$platform_install_path_prefix=hiera('toas::idp::platform_install_path_prefix')
+	$platform_install_path=hiera('toas::idp::platform_install_path')
+	$idp_shibboleth_idp_dir_prefix=hiera('toas::idp::idp_shibboleth_idp_dir_prefix')
+	$idp_install_path=hiera('toas::idp::idp_install_path')
+	$idp_path=hiera('toas::idp::idp_path')
+	$idp_rpm_name=hiera('toas::idp::idp_rpm_name')
+	$idp_rpm=hiera('toas::idp::idp_rpm')
+	$idp_install_script_prefix=hiera('toas::idp::idp_install_script_prefix')
+	$idp_install_script_conf_file=hiera('toas::idp::idp_install_script_conf_file')
+	$idp_install_script=hiera('toas::idp::idp_install_script')
+	$apacheds_rpm=hiera('toas::idp::apacheds_rpm')
 
-	# Dynamic
-	$idp_hostname=hiera('toas::idp::platform_home_prefix')
-    $idp_keystore_password = hiera('toas::idp::platform_home_prefix')
+	# Dynamic(?)
+	$idp_hostname=hiera('toas::idp::idp_hostname')
+    $idp_keystore_password = hiera('toas::idp::idp_keystore_password')
 
-    # Should be from Hiera
-	$platform_name=hiera('toas::idp::platform_home_prefix')
-	$idp_shibboleth_version=hiera('toas::idp::platform_home_prefix')
-	$platform_version=hiera('toas::idp::platform_home_prefix')
-	${apacheds_version}=hiera('toas::idp::platform_home_prefix')
+    
+	$platform_name=hiera('toas::idp::platform_name')
+	$idp_shibboleth_version=hiera('toas::idp::idp_shibboleth_version')
+	$platform_version=hiera('toas::idp::platform_version')
+	$apacheds_version=hiera('toas::idp::apacheds_version')
 }
