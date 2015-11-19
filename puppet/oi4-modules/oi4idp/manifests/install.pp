cef66296a9e03da695b5707b97c7315a5b1c3e36
Ville Korhonen <ville.korhonen@tieto.com>
2015-11-19 15:27:49 +0200

    build problem fixes

--- puppet/oi4-modules/oi4idp/manifests/install.pp
+++ puppet/oi4-modules/oi4idp/manifests/install.pp
@@ -20,7 +20,8 @@ class apacheds::install{
         require oi4idp::params
 
         #$apacheds_rpm="${oi4idp::params::apacheds_rpm}"
-
+		$apacheds_rpm=hiera('toas::idp::apacheds_rpm')
+		
         package { ["${$apacheds_rpm}"]:
                 ensure => installed,
         } 
