7fd151f8b4f836cbf69a0cd8cce105f2a0d9faa0
pekkaraty <pekka.raty@tieto.com>
2015-11-19 14:06:53 +0200

    typo

--- puppet/oi4-modules/profiles/manifests/bas.pp
+++ puppet/oi4-modules/profiles/manifests/bas.pp
@@ -4,7 +4,7 @@ class profiles::bas {
   $extra_catalina_opts = hiera('toas::bas::extra_catalina_opts', undef)
   $oi_home = hiera('toas::oi_home', '/opt/openinfinity')
   $ignore_catalina_propeties = hiera('toas::bas::ignore_catalina_properties', undef) # if bas acts as a base module and some other module provides catalina.properties
-  $run_tomcat_service => hiera('toas::bas:runtomcat', true)  #if bas acts as a base for other module that starts tomcat instead of bas
+  $run_tomcat_service = hiera('toas::bas:runtomcat', true)  #if bas acts as a base for other module that starts tomcat instead of bas
 
   class { 'tomcat':
     install_from_source => false,
