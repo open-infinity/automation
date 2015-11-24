b84642fa770529fd647665a0bb96b3477ea32e1d
Ville Korhonen <ville.korhonen@tieto.com>
2015-11-24 11:33:57 +0200

    oi4idp changes

--- puppet/oi4-modules/profiles/manifests/idp.pp
+++ puppet/oi4-modules/profiles/manifests/idp.pp
@@ -1,6 +1,8 @@
 class profiles::idp {
-  class {'oi4idp::install': 
-  }->
-  class {'oi4idp::config':
+  class {'oi4idp::init': 
   }
+  #class {'oi4idp::install': 
+  #}->
+  #class {'oi4idp::config':
+  #}
 }
