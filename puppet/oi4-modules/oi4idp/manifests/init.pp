9197b7c8279c3ed7bea8393dc97ef5a1c07e72ee
Ville Korhonen <ville.korhonen@tieto.com>
2015-12-02 14:37:54 +0200

    oi4 idp changes

--- puppet/oi4-modules/oi4idp/manifests/init.pp
+++ puppet/oi4-modules/oi4idp/manifests/init.pp
@@ -1,7 +1,8 @@
 class oi4idp {
 		notify {"at oi4idp, starging oi4idp install":}
 		include profiles::base
-		require oi4bas::install
+		#require oi4bas::install
+		require oi4serviceplatform::install
 		class {'oi4idp::install':
 		}->
 		class {'oi4idp::config':
