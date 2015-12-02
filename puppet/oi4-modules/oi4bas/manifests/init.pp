28d32b674cd0da2cc65a4e3c47b466dcff9f0fc3
Ville Korhonen <ville.korhonen@tieto.com>
2015-12-02 15:30:39 +0200

    fixed class name references

--- puppet/oi4-modules/oi4bas/manifests/init.pp
+++ puppet/oi4-modules/oi4bas/manifests/init.pp
@@ -1,6 +1,6 @@
 class oi4bas {
-        include oi4-bas::install
-        include oi4-bas::config
-        include oi4-bas::service
+        include oi4bas::install
+        include oi4bas::config
+        include oi4bas::service
 }
 
