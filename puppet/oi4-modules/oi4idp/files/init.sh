4d8c97b2a896216369ed77adf3c4ed95e8fff4a1
Ville Korhonen <ville.korhonen@tieto.com>
2015-12-08 15:30:18 +0200

    oi4 idp changes

--- puppet/oi4-modules/oi4idp/files/init.sh
+++ puppet/oi4-modules/oi4idp/files/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # install ant-contrib (needed by shibboleth idp installation)
 echo installing ant contrib:
+cd /root
 yum install wget
 wget http://downloads.sourceforge.net/project/ant-contrib/ant-contrib/ant-contrib-1.0b2/ant-contrib-1.0b2-bin.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fant-contrib%2Ffiles%2Fant-contrib%2Fant-contrib-1.0b2%2F&ts=1448885065&use_mirror=netassist
 mv ant-contrib-1.0b2-bin.tar.gz\?r\=http\:%2F%2Fsourceforge.net%2Fprojects%2Fant-contrib%2Ffiles%2Fant-contrib%2Fant-contrib-1.0b2%2F ant-contrib-1.0b2-bin.tar.gz
