7d6506e08f4ce4108ad7453abbb6ff698177fca6
Ville Korhonen <ville.korhonen@tieto.com>
2015-12-10 13:08:06 +0200

    changed jstl jar retrieval

--- puppet/oi4-modules/oi4idp/files/init-tomcat.sh
+++ puppet/oi4-modules/oi4idp/files/init-tomcat.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+cd /opt/openinfinity/tomcat/lib/opt/
 wget https://build.shibboleth.net/nexus/service/local/repositories/thirdparty/content/javax/servlet/jstl/1.2/jstl-1.2.jar
-cp jstl-1.2.jar  /opt/openinfinity/tomcat/lib/opt/
+#cp jstl-1.2.jar  /opt/openinfinity/tomcat/lib/opt/
