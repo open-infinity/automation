19d4b5688c0f3a8fc43f9269e2c3edb5d2489a31
pekkaraty <pekka.raty@tieto.com>
2016-01-20 13:51:52 +0200

    update

--- puppet/oi4-modules/profiles/manifests/nosql.pp
+++ puppet/oi4-modules/profiles/manifests/nosql.pp
@@ -1,9 +1,9 @@
 class profiles::nosql {
+	$mongod_replicaset_name=hiera('toas::mongod::mongod_replicaset_name')
     $mongo_cluster_type=hiera('toas::mongod::mongo_cluster_type')
 	$_mongod_port=hiera('toas::mongod::mongod_port')
 	$_mongo_storage_smallFiles=hiera('toas::mongod::mongo_storage_smallFiles')
 	$_mongo_security_authorization=hiera('toas::mongod::mongo_security_authorization')
-	$mongod_replicaset_name=hiera('toas::mongod::mongod_replicaset_name')
 	$mongod_replicaset_oplogSizeMB=hiera('toas::mongod::mongod_replicaset_oplogSizeMB')
 	$mongod_replicaset_node=hiera('toas::mongod::mongod_replicaset_node')
 	$mongo_mongos_node=hiera('toas::mongod::mongo_mongos_node')
