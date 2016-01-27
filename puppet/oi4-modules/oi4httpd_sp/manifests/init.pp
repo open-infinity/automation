ed0a28da2e1c93b3c49d60c9924daa133d76a4e3
Ville Korhonen <ville.korhonen@tieto.com>
2016-01-27 15:47:41 +0200

    fixed duplicate variable assignment

--- puppet/oi4-modules/oi4httpd_sp/manifests/init.pp
+++ puppet/oi4-modules/oi4httpd_sp/manifests/init.pp
@@ -128,7 +128,7 @@ class oi4httpd_sp::config inherits oi4variables {
     }
 
 	$sp_root_rsa_key_public=hiera('toas::sp::sp_root_rsa_key_public')
-	$httpd_domain_name=hiera('toas::sp::httpd_domain_name')
+	#$httpd_domain_name=hiera('toas::sp::httpd_domain_name')
 	$sp_root_rsa_key_private=hiera('toas::sp::sp_root_rsa_key_private')
 	#$sp_root_rsa_key_private = PSON.parse(hiera('toas::sp::sp_root_rsa_key_private'))
 	
