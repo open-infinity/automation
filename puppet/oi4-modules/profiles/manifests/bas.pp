f6aa13069dde80dab17d85f8278ab2875741bc4b
Vedran Bartonicek <vbartoni@gmail.com>
2016-02-25 18:56:03 +0200

    Fix for bas connectors - enforcing ajp
    -previous fix used wrong protocols for tomcat Connectors configuraion
    -new hiera parameter for swithching on/off ajp enforcing

--- puppet/oi4-modules/profiles/manifests/bas.pp
+++ puppet/oi4-modules/profiles/manifests/bas.pp
@@ -7,24 +7,23 @@ class profiles::bas {
   $run_tomcat_service = hiera('toas::bas:runtomcat', true)  #if bas acts as a base for other module that starts tomcat instead of bas
   $bas_hazelcast_cluster_nodes = hiera('toas::bas:hazelcast::nodes', undef)
 
-# Session attribute identifiers
-$sso_attribute_session_identifier = hiera('sso::attribute::session::identifier', 'Shib-Session-ID')
-$sso_attribute_session_username = hiera('sso::attribute::session::username','uid')
-$sso_attribute_session_tenant_id = hiera('sso::attribute::session::tenant::id','tenantId')
-$sso_attribute_session_roles = hiera('sso::attribute::session::roles','roles')
-$sso_attribute_session_role_delimiter = hiera('sso::attribute::session::role::delimiter',',')
-$sso_attribute_session_attributes = hiera('sso::attribute::session::attributes','name,address,phone')
-$sso_attribute_session_user_attribute_delimiter = hiera('sso::attribute::session::user::attribute::delimiter', ',')
+  # Session attribute identifiers
+  $sso_attribute_session_identifier = hiera('sso::attribute::session::identifier', 'Shib-Session-ID')
+  $sso_attribute_session_username = hiera('sso::attribute::session::username','uid')
+  $sso_attribute_session_tenant_id = hiera('sso::attribute::session::tenant::id','tenantId')
+  $sso_attribute_session_roles = hiera('sso::attribute::session::roles','roles')
+  $sso_attribute_session_role_delimiter = hiera('sso::attribute::session::role::delimiter',',')
+  $sso_attribute_session_attributes = hiera('sso::attribute::session::attributes','name,address,phone')
+  $sso_attribute_session_user_attribute_delimiter = hiera('sso::attribute::session::user::attribute::delimiter', ',')
 
-# Session header identifiers
-$sso_header_session_identifier = hiera('sso::header::session::identifier','iv-user')
-$sso_header_session_username = hiera('sso::header::session::username','iv-user')
-$sso_header_session_tenant_id = hiera('sso::header::session::tenant::id','iv-groups')
-$sso_header_session_roles = hiera('sso::header::session::roles','iv-groups')
-$sso_header_session_role_delimiter = hiera('sso::header::session::role::delimiter',',')
-$sso_header_session_attributes = hiera('sso::header::session::attributes','name,address,phone')
-$sso_header_session_user_attribute_delimiter = hiera('sso::header::session::user::attribute::delimiter',',')
-
+  # Session header identifiers
+  $sso_header_session_identifier = hiera('sso::header::session::identifier','iv-user')
+  $sso_header_session_username = hiera('sso::header::session::username','iv-user')
+  $sso_header_session_tenant_id = hiera('sso::header::session::tenant::id','iv-groups')
+  $sso_header_session_roles = hiera('sso::header::session::roles','iv-groups')
+  $sso_header_session_role_delimiter = hiera('sso::header::session::role::delimiter',',')
+  $sso_header_session_attributes = hiera('sso::header::session::attributes','name,address,phone')
+  $sso_header_session_user_attribute_delimiter = hiera('sso::header::session::user::attribute::delimiter',',')
 
 
   class { 'tomcat':
@@ -33,41 +32,42 @@ $sso_header_session_user_attribute_delimiter = hiera('sso::header::session::user
     group               => 'oiuser',
     manage_user         => false,
     manage_group        => false,
-	catalina_home		=> "${oi_home}/tomcat",
+    catalina_home       => "${oi_home}/tomcat",
   }
-  file {"$oi_home/log/tomcat":
+  file { "$oi_home/log/tomcat":
     ensure  => 'directory',
     owner   => 'oiuser',
     group   => 'oiuser',
     mode    => 755,
     require => [User["oiuser"], File["${oi_home}/log"]],
   }->
-  class {'oi4bas::install':
+  class { 'oi4bas::install':
   }->
-  class {'oi4bas::config':
-    bas_multicast_address       => $multicast_address,
-    bas_tomcat_monitor_role_pwd => $tomcat_monitor_role_password,
-	ignore_catalina_propeties => $ignore_catalina_propeties,
-	sso_attribute_session_identifier => $sso_attribute_session_identifier,
-	sso_attribute_session_username => $sso_attribute_session_username,
-	sso_attribute_session_tenant_id => $sso_attribute_session_tenant_id,
-	sso_attribute_session_roles => $sso_attribute_session_roles,
-	sso_attribute_session_role_delimiter => $sso_attribute_session_role_delimiter,
-	sso_attribute_session_attributes => $sso_attribute_session_attributes,
-	sso_attribute_session_user_attribute_delimiter => $sso_attribute_session_user_attribute_delimiter,
-	sso_header_session_identifier => $sso_header_session_identifier,
-	sso_header_session_username => $sso_header_session_username,
-	sso_header_session_tenant_id => $sso_header_session_tenant_id,
-	sso_header_session_roles => $sso_header_session_roles,
-	sso_header_session_role_delimiter => $sso_header_session_role_delimiter,
-	sso_header_session_attributes => $sso_header_session_attributes,
-	sso_header_session_user_attribute_delimiter => $sso_header_session_user_attribute_delimiter, 
-	bas_hazelcast_cluster_nodes => $bas_hazelcast_cluster_nodes
-	
-  }->class {'profiles::bas::tomcatconf':
-	oi_home => $oi_home
-  }->class {'oi4bas::service': 
-	run_tomcat => $run_tomcat_service
+  class { 'oi4bas::config':
+    bas_multicast_address                          => $multicast_address,
+    bas_tomcat_monitor_role_pwd                    => $tomcat_monitor_role_password,
+    ignore_catalina_propeties                      => $ignore_catalina_propeties,
+    force_ajp                                      => $force_ajp,
+    sso_attribute_session_identifier               => $sso_attribute_session_identifier,
+    sso_attribute_session_username                 => $sso_attribute_session_username,
+    sso_attribute_session_tenant_id                => $sso_attribute_session_tenant_id,
+    sso_attribute_session_roles                    => $sso_attribute_session_roles,
+    sso_attribute_session_role_delimiter           => $sso_attribute_session_role_delimiter,
+    sso_attribute_session_attributes               => $sso_attribute_session_attributes,
+    sso_attribute_session_user_attribute_delimiter => $sso_attribute_session_user_attribute_delimiter,
+    sso_header_session_identifier                  => $sso_header_session_identifier,
+    sso_header_session_username                    => $sso_header_session_username,
+    sso_header_session_tenant_id                   => $sso_header_session_tenant_id,
+    sso_header_session_roles                       => $sso_header_session_roles,
+    sso_header_session_role_delimiter              => $sso_header_session_role_delimiter,
+    sso_header_session_attributes                  => $sso_header_session_attributes,
+    sso_header_session_user_attribute_delimiter    => $sso_header_session_user_attribute_delimiter,
+    bas_hazelcast_cluster_nodes                    => $bas_hazelcast_cluster_nodes
+
+  }->class { 'profiles::bas::tomcatconf':
+    oi_home => $oi_home
+  }->class { 'oi4bas::service':
+    run_tomcat => $run_tomcat_service
   }
 }
 
@@ -75,27 +75,30 @@ class  profiles::bas::tomcatconf  ( $oi_home = undef ) {
   $extra_jvm_opts = hiera('toas::bas::extra_jvm_opts', undef)
   $jvm_mem = hiera('toas::bas::jvm_mem')
   $jvm_perm = hiera('toas::bas::jvm_perm')
+  $force_ajp = hiera('toas::bas::force_ajp')
 
-  tomcat::config::server::connector { 'tomcat_ajp_8089':
-    catalina_base         => "${oi_home}/tomcat",
-    port                  => '8009',
-    additional_attributes => {
-    'redirectPort' => '443'
-    },
-    connector_ensure => 'present',
-    purge_connectors => true
-  }
+  if $force_ajp {
+    tomcat::config::server::connector { 'AJP/1.3':
+      catalina_base         => "${oi_home}/tomcat",
+      port                  => '8009',
+      additional_attributes => {
+        'redirectPort' => '443'
+      },
+      connector_ensure      => 'present',
+      purge_connectors      => true
+    }
 
-  tomcat::config::server::connector { 'tomcat_http_8080':
-    catalina_base         => "${oi_home}/tomcat",
-    port                  => '8080',
-    connector_ensure => 'absent',
-  }
+    tomcat::config::server::connector { 'HTTP/1.1':
+      catalina_base         => "${oi_home}/tomcat",
+      port                  => '8080',
+      connector_ensure      => 'absent',
+    }
 
-  tomcat::config::server::connector { 'tomcat_https_443':
-    catalina_base         => "${oi_home}/tomcat",
-    port                  => '443',
-    connector_ensure => 'absent',
+#    tomcat::config::server::connector { 'tomcat_https_443':
+#      catalina_base         => "${oi_home}/tomcat",
+#      port                  => '443',
+#      connector_ensure      => 'absent',
+#    }
   }
 
   tomcat::config::server::valve { 'securityvault-valve':
@@ -111,12 +114,12 @@ class  profiles::bas::tomcatconf  ( $oi_home = undef ) {
     value => "${oi_home}/log/tomcat/catalina.out",
   }
 
-  tomcat::setenv::entry {'SECURITY_VAULT_OPTS':
+  tomcat::setenv::entry { 'SECURITY_VAULT_OPTS':
     param => 'SECURITY_VAULT_OPTS',
     value => "-Dsecurity.vault.properties.file=/opt/openinfinity/tomcat/conf/securityvault.properties",
   }
 
-  tomcat::setenv::entry {'jmx_opts':
+  tomcat::setenv::entry { 'jmx_opts':
     param => 'JMX_OPTS',
     value => "\"-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=65329 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=true -Dcom.sun.management.jmxremote.password.file=$oi_home/tomcat/conf/jmxremote.password -Dcom.sun.management.jmxremote.access.file=$oi_home/tomcat/conf/jmxremote.access\"",
     order => 1,
