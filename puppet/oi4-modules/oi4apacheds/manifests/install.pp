41151c6458c3ef857628887717beb0d5a0b3de5d
Ville Korhonen <ville.korhonen@tieto.com>
2016-03-17 12:28:14 +0200

    Added check to avoid error when java package already exists.

--- puppet/oi4-modules/oi4apacheds/manifests/install.pp
+++ puppet/oi4-modules/oi4apacheds/manifests/install.pp
@@ -1,7 +1,10 @@
 class oi4apacheds::install{
-	package { ["java-1.8.0-openjdk"]:
-		ensure => present
+	if ! defined(Package['java-1.8.0-openjdk']) {
+		package { ["java-1.8.0-openjdk"]:
+			ensure => present
+		}
 	}
+	
 	package { "apacheds":
         	ensure => installed,
         }
