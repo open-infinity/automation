7264308fb19026ed41310df9a7bf0f44e68cb6ea
Ville Korhonen <ville.korhonen@tieto.com>
2016-04-20 15:58:29 +0300

    Corrected adding of lines to /etc/hosts

--- puppet/oi4-modules/oi4os/manifests/config.pp
+++
@@ -1,9 +0,0 @@
-class oi4os::config (
-)  {
-	$etc_hosts_additions=hiera('toas::os::etc_hosts_additions', undef)
-	file_line { 'Append a line to /etc/hosts':
-		path  => '/etc/hosts', 
-		line => $etc_hosts_additions,
-	}
-}
-
--- puppet/oi4-modules/oi4os/manifests/init.pp
+++ puppet/oi4-modules/oi4os/manifests/init.pp
@@ -1,18 +1,17 @@
 class oi4os {
-	$etc_hosts_additions=hiera('toas::os::etc_hosts_additions', undef)
-	
-	file { '/root/append_to_etc_hosts.sh':
-		content => template("oi4os/append_to_etc_hosts.sh.erb"),
-		ensure => present,
-		replace => true,
-		owner => "root",
-		group => "root",
-    mode => 0700,
-	} -> 
-	exec { 'append_to_etc_hosts':		
-		command => '/root/append_to_etc_hosts.sh',
-		user    => 'root',
-		group   => 'root',
-	}
-}
+        $etc_hosts_additions=hiera('toas::os::etc_hosts_additions', undef)
 
+        file { '/root/append_to_etc_hosts.sh':
+                content => template("oi4os/append_to_etc_hosts.sh.erb"),
+                ensure => present,
+                replace => true,
+                owner => "root",
+                group => "root",
+                mode => 0700,
+        } ->
+        exec { 'append_to_etc_hosts':
+                command => "/root/append_to_etc_hosts.sh",
+                user    => 'root',
+                group   => 'root',
+        }
+}
